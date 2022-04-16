498dd9543d430f454cd1fedff7cf9cbe853f383e
Gilberto Taccari <gilberto.taccari@gmail.com>
2022-04-16 08:01:24 +0200

    Setup basic Spring Cloud Function project

---
+++ src/main/java/it/tccr/experiment/awslambdatest/AwsLambdaTestApplication.java
@@ -0,0 +1,13 @@
+package it.tccr.experiment.awslambdatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaTestApplication.class, args);
+	}
+
+}
